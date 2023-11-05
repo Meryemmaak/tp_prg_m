23210e3a1ecfc40d94c6f3dba423960ee925eff6
meryem <meryem.maakoufi@gmail.com>
2023-11-05 13:38:27 +0100

    Création des clients du template

---
+++ prgTP/src/main/java/com/meryy/prgtp/jsf/Bean4.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.meryy.prgtp.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author hp
+ */
+@Named(value = "bean4")
+@RequestScoped
+public class Bean4 {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Cette méthode retourne une liste de nombres suivants à partir du "nombre"
+     * saisi.
+     *
+     * @return
+     */
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    /**
+     * Méthode "afficher" qui génère une chaîne de caractères pour la
+     * redirection. Elle renvoie une chaîne représentant l'URL de redirection, y
+     * compris le paramètre "nb" avec la valeur de "nombre".
+     *
+     * @return
+     */
+    public String afficher() {
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+}
